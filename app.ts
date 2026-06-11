3313e19838953784d137c4c4a6b1ac52a80825ee
Anthony <anthonnhani@gmail.com>
2026-06-11 14:27:37 -0600

    Set up the MVC skeleton of the project

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "node:path";
+import router from "./routes/index.route";
+const app = express();
+
+// Setting up view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// Middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use(router);
+
+// Listening to port
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
