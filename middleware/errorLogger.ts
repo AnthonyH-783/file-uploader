8dd162b7e400d7aea200226f3788a428d6d2c241
Anthony <anthonnhani@gmail.com>
2026-08-05 10:53:08 +0300

    Added special case for P2025 error under global error middleware

--- middleware/errorLogger.ts
+++ middleware/errorLogger.ts
@@ -1,8 +1,15 @@
 import {Request, Response, NextFunction} from "express";
 import { AppError } from "../errors/AppError";
+import { PrismaClientKnownRequestError } from "../generated/prisma/internal/prismaNamespace";
 
 export const errorLogger = (err: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
+
+    if(err instanceof PrismaClientKnownRequestError){
+        if(err.code == "P2025"){
+            return res.status(404).json({error: "Ressource not found"});
+        }
+    }
     if (err instanceof AppError) {
         res.status(err.statusCode).json({ message: err.message });
         return;
